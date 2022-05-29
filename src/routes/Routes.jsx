d5114e1b5a6292a67f0b07e260fb0dd85a122739
Aditya Himawan <30428194+adityahimaone@users.noreply.github.com>
2022-05-29 11:48:42 +0700

    fix import routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "@/pages/index";
-import Projects from "@/pages/projects";
+import Index from "../pages/index";
+import Projects from "../pages/projects";
 import DefaultLayout from "@/components/Layouts/Default";
 
 export default function RoutesPage() {
